bbe267a822ceba57dc8d3d7b0e297a905d40ed9e
david.moya@mayoral.com <david.moya@mayoral.com>
2023-01-11 11:31:18 +0100

    update 11/01 11:31

--- Módulo 1 - Fundamentos en linux/Ejercicios/Ejercicios CLI/5/5.sh
+++ Módulo 1 - Fundamentos en linux/Ejercicios/Ejercicios CLI/5/5.sh
@@ -1,26 +1,35 @@
 #!/bin/bash
 set -e
 
-DEFAULT="Introduce una variable!!"
-VAR1=${1:-$DEFAULT}
 NWORDS=0
+FICHERO=comands.txt
 
 if [ $# -lt "2" ];then
-        echo "Debes introducir dos parámetros"
-        exit 1
+   echo "Debes introducir dos parámetros"
+   exit 1
 fi
-##Una vez comprobamos que el numero de parámetros es correcto, procedemos a hacer el wget del server. En mi caso he subido un archivo .txt a un hfs para la prueba.
-wget $2
+
+##Una vez comprobamos que el numero de parámetros es correcto, procedemos a confirmar si el fichero existe, de ser así no se hace el wget del server. En mi caso he subido un archivo .txt a un hfs para la prueba.
+if ! [ -f "$FICHERO" ]; then
+   wget http://$1/$FICHERO
+else 
+   read -p "El $FICHERO existe, ¿desea borrarlo y volver a descargarlo?[yes:no]" RES
+   if [ $RES = "yes" ]; then
+      rm -rf $FICHERO
+      wget http://$1/$FICHERO
+   fi
+fi
+
 ## En esta parte añadimos dos variables las cuales nos dan los valores que necesitamos, nº de veces que se repite la palabra en el documento y la primera linea en la que aparece dicha palabra. 
-NWORDS=$(cat comands.txt | grep -o $1 | wc -l)
-FLINE=$(cat comands.txt | grep -n $1 | awk -F ":" 'NR==1{print $1}')
+NWORDS=$(cat $FICHERO | grep -o $2 | wc -l)
+FLINE=$(cat $FICHERO | grep -n $2 | awk -F ":" 'NR==1{print $1}')
 ##Sentencia if donde haremos la comprobación, si es mayor que 1 haz esto, si es igual a 1 haz lo otro y por último si no es ninguno de estos dos no aparece la palabra en el fichero.
 if [[ $NWORDS -gt 1 ]]; then
-   echo "La palabra $1 aparece $NWORDS veces"
-   echo "La palabra $1 aparece por primera vez en la linea $FLINE"
+   echo "La palabra $2 aparece $NWORDS veces"
+   echo "La palabra $2 aparece por primera vez en la linea $FLINE"
 elif [[ $NWORDS -eq 1 ]]; then
-   echo "La palabra $1 aparece $NWORDS"
-   echo "La palabra $1 aparece por primera vez en la linea $FLINE"
+   echo "La palabra $2 aparece $NWORDS veces"
+   echo "La palabra $2 aparece por primera vez en la linea $FLINE"
 else
-   echo "La palabra $1 no aparece en el fichero"
+   echo "La palabra $2 no aparece en el fichero $FICHERO"
 fi
