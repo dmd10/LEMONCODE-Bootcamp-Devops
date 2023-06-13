10e27f41853ea73570c805644474173a14d98ced
David Moya Durán <dmd@PC075224.dominiomayoral.es>
2023-06-13 16:05:49 +0200

    update

--- Módulo 1 - Fundamentos en linux/Ejercicios/Ejercicios CLI/3/script.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-DEFAULT="Me encanta bash!!"
-VAR1=${1:-$DEFAULT}
-
-mkdir -p foo/{dummy,empty} && echo $VAR1 > foo/dummy/file1.txt && touch foo/dummy/file2.txt
-
-cat foo/dummy/file1.txt > foo/dummy/file2.txt && mv foo/dummy/file2.txt foo/empty/
